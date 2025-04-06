bfbea85d014422feef735756cab9b4b38a620494
Alexander Graedel <algraden@amazon.ch>
2025-04-06 12:03:37 +0200

    init style

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   site: SITE.website,
   integrations: [
     tailwind({
-      applyBaseStyles: false,
+      applyBaseStyles: true,
     }),
     react(),
     sitemap(),
